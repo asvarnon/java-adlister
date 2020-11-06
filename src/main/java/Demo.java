883ddcd251edd51d9c21b087059fe36f80353010
Austin Varnon <austinvarnon@gmail.com>
2020-11-06 16:57:08 -0600

    turning table data from mysql into arraylists in java

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 import com.mysql.cj.api.mysqla.result.Resultset;
 import com.mysql.cj.jdbc.Driver;
@@ -6,8 +7,9 @@ import com.mysql.cj.jdbc.Driver;
 public class Demo {
     public static void main(String[] args) {
 
-        long idToUpdate = 36;
+        long idToUpdate = 2;
         Config config = new Config();
+        ArrayList<Album> albums = new ArrayList<>();
 
         try {
             DriverManager.registerDriver(new Driver());
@@ -21,14 +23,14 @@ public class Demo {
             ResultSet rs = statement.executeQuery("SELECT * FROM albums");
 
 //            insert example
-            String sql = "INSERT INTO albums (artist, name, release_date, sales, genre) VALUES('Nelly Furtado', 'Folklore', 2003, 50, 'Pop')";
+//            String sql = "INSERT INTO albums (artist, name, release_date, sales, genre) VALUES('Nelly Furtado', 'Folklore', 2003, 50, 'Pop')";
 
-            long latestID = statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
-            System.out.println("latestID = " + latestID);
-            rs = statement.getGeneratedKeys();
-            if (rs.next()) {
-                System.out.println("Inserted a new record! New id: " + rs.getLong(1));
-            }
+//            long latestID = statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+//            System.out.println("latestID = " + latestID);
+//            rs = statement.getGeneratedKeys();
+//            if (rs.next()) {
+//                System.out.println("Inserted a new record! New id: " + rs.getLong(1));
+//            }
 
 
 
@@ -43,13 +45,41 @@ public class Demo {
 //              LOOP THROUGH EXAMPLE
 //            .next() will check if there is a next result as well.
             while(rs.next()){
-                System.out.println("rs.getLong(\"id\") = " + rs.getLong("id"));
-                System.out.println("rs.getString(3) = " + rs.getString(3));
+                albums.add(new Album(
+                        rs.getLong("id"),
+                        rs.getString("artist"),
+                        rs.getString("name"),
+                        rs.getInt("release_date"),
+                        rs.getDouble("sales"),
+                        rs.getString("genre")
+                        )
+                );
+
+                for (Album album : albums){
+                    System.out.println("album.getId() = " + album.getId());
+                    System.out.println("album.getArtist() = " + album.getArtist());
+                    System.out.println("album.getName() = " + album.getName());
+                    System.out.println("album.getRelease_date() = " + album.getRelease_date());
+                    System.out.println("album.getSales() = " + album.getSales());
+                    System.out.println("album.getGenre() = " + album.getGenre());
+
+                }
+
+//                album.setId(rs.getLong("id"));
+//                album.setName(rs.getString(3));
+//                album.setSales(rs.getDouble("sales"));
+
+//                System.out.println("album.getId() = " + album.getId());
+//                System.out.println("album.getName() = " + album.getName());
+//                System.out.println("album.getSales() = " + album.getSales());
+
+//                System.out.println("rs.getLong(\"id\") = " + rs.getLong("id"));
+//                System.out.println("rs.getString(3) = " + rs.getString(3));
             }
 
 //            UPDATE EXAMPLE
-            double sales = 60.5;
-            statement.executeUpdate("UPDATE albums SET sales = " + sales + " WHERE id = " + idToUpdate);
+//            double sales = 60.5;
+//            statement.executeUpdate("UPDATE albums SET sales = " + sales + " WHERE id = " + idToUpdate);
 
 
 
